329cf61a91e73b22251424181c9302cf2edd7355
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-18 15:20:46 +0200

    add grafic show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,28 +3,48 @@
 @section('title', 'Appartamento')
 
 @section('content')
-  <h1 class="text-center">{{$apartment->title}}</h1>
+  <h1 class="text-center my-5">{{$apartment->title}}</h1>
+  <hr>
   <div class="d-flex justify-content-center">
-    <div class="card mb-3" >
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="{{$apartment->cover}}" class="img-fluid rounded-start" alt="">
+    
+      <div class="row g-3">
+        <div class="col-md-5">
+          <img src="{{$apartment->cover}}" class="img-fluid" alt="">
         </div>
-        <div class="col-md-8">
+        <div class="col-md-7">
           <div class="card-body">
-            <h5 class="card-title">{{$apartment->title}}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p><strong>Camere: </strong>{{$apartment->rooms}}</p>
-            <p><strong>Letti: </strong>{{$apartment->beds}}</p>
-            <p><strong>Bagni: </strong>{{$apartment->bathrooms}}</p>
-            <p><strong>Metri quadri:</strong>{{$apartment->sqm}}</p>
-            <p><strong>Indirizzo: </strong>{{$apartment->address}}</p>
-            <p><strong>Pubblicato: </strong>{{$apartment->is_visible}}</p>
-            <a href="{{route('admin.apartments.index')}}" class="btn btn-primary">Torna indietro</a>
-            <a href="{{route('admin.apartments.edit', $apartment->id)}}" class="btn btn-sm btn-warning"><i class="fa-regular fa-pen-to-square"></i></a>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum aliquid, sunt autem error cupiditate unde hic inventore repellat, accusamus eius blanditiis ipsa pariatur sint illum, quia in dolorem impedit debitis.
+            Accusantium, iure. Eos expedita quae sapiente? Totam aut quam consequuntur, soluta, facere, debitis culpa expedita ipsum ducimus tempore in omnis. Ut, consequuntur earum dicta provident ullam nisi perspiciatis rem laborum.
+            Magnam iure cum laudantium eos fugit, quaerat qui ipsa excepturi ipsum fugiat perspiciatis voluptas expedita animi aut accusantium quo nemo. Architecto corrupti praesentium ipsa dignissimos sit obcaecati quod at dicta!
+            Temporibus fugiat eligendi commodi quaerat labore, placeat tempora perspiciatis soluta, beatae debitis assumenda unde reiciendis, est aperiam necessitatibus aspernatur amet fuga? Deserunt accusantium voluptate obcaecati mollitia consequuntur alias debitis vero.
+            </p>
           </div>
         </div>
+        <div class="col">
+          <div class="card-info">
+            <div class="row">
+              <h2 class="my-1">Info</h2>
+              <hr>
+              <div class="col my-4 d-flex justify-content-around">
+                <div><i class="fa-solid fa-door-closed"></i> Stanze: <strong>{{$apartment->rooms}}</strong></div>
+                <div><i class="fa-solid fa-bed"></i> Letti: <strong>{{$apartment->beds}}</strong></div>
+                <div><i class="fa-solid fa-bath"></i> Bagni: <strong>{{$apartment->bathrooms}}</strong></div>
+                <div><i class="fa-solid fa-house"></i> <strong>{{$apartment->sqm}}</strong> mq2</div>
+                <div><i class="fa-solid fa-location-dot"></i> Indirizzo: <strong>{{$apartment->address}}</strong></div>
+            </div>
+          </div>
+          <div class="row">
+            <h2 class="my-1">Servizi</h2>
+            <hr>
+            <div class="col my-4 d-flex justify-content-around">
+              <div><i class="fa-solid fa-wifi"></i> wi-fi: <strong>{{$apartment->rooms}}</strong></div>
+              
+            </div>
+          </div>
+          <div class="d-flex justify-content-end align-items-center gap-2 mt-3">
+            <a href="{{route('admin.apartments.index')}}" class="btn btn-primary">Torna indietro</a>
+            <a href="{{route('admin.apartments.edit', $apartment->id)}}" class="btn btn-warning"><i class="fa-regular fa-pen-to-square"></i> Modifica</a>
+          </div>
       </div>
-    </div>
   </div>
 @endsection
