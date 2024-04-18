2b5f178780c6bd02137bacdf0718c1681a926327
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-18 17:16:42 +0200

    added comments into index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -8,11 +8,15 @@
   <h3 class="mb-0">I tuoi appartamenti</h3>
 
   <div>
+    {{-- Aggiungi appartamento --}}
     <a href="{{route('admin.apartments.create')}}" class="btn btn-light bg-base-color me-2 rounded-4 text-white">Aggiungi appartamento <i class="fa-solid fa-house-medical ms-1"></i></a>
+
+    {{-- Cestino --}}
     <a class="btn btn-light border round-50" href=""><i class="fa-regular fa-trash-can"></i></a>  
   </div>
 </div>
 
+  {{-- Tabella appartamenti --}}
   <table class="table table-hover">
     <thead>
       <tr>
@@ -28,23 +32,41 @@
     <tbody>
       @forelse ($apartments as $apartment)
         <tr>
+
+          {{-- Cover --}}
           <td class="text-start"><img src="{{$apartment->cover}}" alt="{{$apartment->title}}" class="img-fluid table-img rounded-1"></td>
+
+          {{-- Titolo --}}
           <td>{{$apartment->title}}</td>
+
+          {{-- Stato --}}
           <td>{{$apartment->is_visible}}</td>
+
+          {{-- Creazione --}}
           <td>{{$apartment->created_at}}</td>
+
+          {{-- Modifica --}}
           <td>{{$apartment->updated_at}}</td>
           <td>
             <div class="d-flex justify-content-end align-items-center gap-1">
-              <a href="{{route('admin.apartments.show', $apartment->id)}}" class="btn btn-sm btn-primary round-50 d-flex align-items-center justify-content-center"><i class="fa-regular fa-eye"></i></a>
+
+              {{-- Show appartamento --}}
+              <a href="{{route('admin.apartments.show', $apartment->id)}}" class="btn btn-sm bg-base-color border border-light text-white round-50 d-flex align-items-center justify-content-center"><i class="fa-regular fa-eye"></i></a>
               <div>
+
+                {{-- Dropdown --}}
                 <button type="button" class="btn btn-light dropdown-toggle round-50 border" data-bs-toggle="dropdown" aria-expanded="false">
                   <i class="fa-solid fa-gear"></i>  
                 </button>
                 <ul class="dropdown-menu p-2">
                   <li>
+
+                    {{-- Form modifica --}}
                     <a href="{{route('admin.apartments.edit', $apartment->id)}}" class="dropdown-item bg-warning text-white rounded-3 mb-2 text-center"><i class="fa-regular fa-pen-to-square"></i> Modifica</a>
                   </li>
                   <li>
+
+                    {{-- Form cancellazione soft --}}
                     <form method="POST" action="{{route('admin.apartments.destroy', $apartment->id)}}">
                       @csrf
                       @method('DELETE')
@@ -53,13 +75,6 @@
                   </li>
                 </ul>
               </div>
-              {{-- <a href="{{route('admin.apartments.show', $apartment->id)}}" class="btn btn-sm btn-primary"><i class="fa-regular fa-eye"></i></a>
-              <a href="{{route('admin.apartments.edit', $apartment->id)}}" class="btn btn-sm btn-warning"><i class="fa-regular fa-pen-to-square"></i></a>
-              <form method="POST" action="{{route('admin.apartments.destroy', $apartment->id)}}">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-sm btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-              </form> --}}
             </div>
           </td>
         </tr>
