a3cfe2ac251a6d61eccbb536d32d7016d94a8db9
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-19 18:08:02 +0200

    added img file

--- config/apartment_services.php
+++ config/apartment_services.php
@@ -1,13 +1,13 @@
 <?php
 return [
     // Essential
-    ['label' => 'Wi-Fi', 'icon' => 'bigtop_updates.png'],
-    ['label' => 'Piscina', 'icon' => 'pool.png'],
-    ['label' => 'Vasca da bagno', 'icon' => 'hot_tub.png'],
-    ['label' => 'Aria condizionata', 'icon' => 'mode_fan.png'],
-    ['label' => 'Parcheggio', 'icon' => 'garage.png'],
-    ['label' => 'Cucina', 'icon' => 'cooking.png'],
-    ['label' => 'Animali domestici ammessi', 'icon' => 'sound_detection_dog_barking.png'],
-    ['label' => 'Lavatrice', 'icon' => 'dishwasher_gen.png'],
-    ['label' => 'TV', 'icon' => 'desktop_windows.png'],
+    ['label' => 'Wi-Fi', 'icon' => 'bigtop_updates.svg'],
+    ['label' => 'Piscina', 'icon' => 'pool.svg'],
+    ['label' => 'Vasca da bagno', 'icon' => 'hot_tub.svg'],
+    ['label' => 'Aria condizionata', 'icon' => 'mode_fan.svg'],
+    ['label' => 'Parcheggio', 'icon' => 'garage.svg'],
+    ['label' => 'Cucina', 'icon' => 'cooking.svg'],
+    ['label' => 'Animali domestici ammessi', 'icon' => 'sound_detection_dog_barking.svg'],
+    ['label' => 'Lavatrice', 'icon' => 'dishwasher_gen.svg'],
+    ['label' => 'TV', 'icon' => 'desktop_windows.svg'],
 ];
