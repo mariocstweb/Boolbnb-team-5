acdd4433ad217c224f41fcd6e16dddb457a042d8
mariocstweb <mariocstmra@gmail.com>
2024-04-21 14:25:19 +0200

    added img and filled login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+{{-- <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+
 @endsection
