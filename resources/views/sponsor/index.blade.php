f5c184c9a4def10fd8d9f48d7ec39d5fffb2b8ea
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-22 17:16:26 +0200

    added grafic sponsor

--- resources/views/sponsor/index.blade.php
+++ resources/views/sponsor/index.blade.php
@@ -4,17 +4,19 @@
 
 @section('content')
   <img src="{{Vite::asset('resources/img/plus.png')}}" alt="" class="img-fluid mt-4">
-  <h5 class="mt-3">Acquista uno dei nostri pacchetti e ottieni dei vantaggi esclusivi sui tuoi appartamenti</h5>
+  <h3 class="my-4">Acquista uno dei nostri pacchetti e ottieni dei vantaggi esclusivi sui tuoi appartamenti</h3>
 
   <div class="row">
     @foreach ($sponsors as $sponsor)
     <div class="col">
-      <div class="card text-white p-3 border-0" style="background-image: linear-gradient(to left,{{$sponsor->color}})">
+      <div class="card text-white p-3 border-0 rounded-4" style="background-image: linear-gradient(to left,{{$sponsor->color}})">
         <div class="card-body">
-          <h5 class="card-title">{{$sponsor->label}}</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">{{$sponsor->description}}</h6>
-          <p class="card-text">{{$sponsor->price}}€ per {{$sponsor->duration}}/h</p>
-          <a href="" class="btn ">Passa a pro</a>
+          <h1 class="card-title mb-3">{{$sponsor->label}}</h1>
+          <p class="card-subtitle mb-4 fs-4">{{$sponsor->description}}</p>
+          <p class="card-text fs-4"><strong class="fs-1">{{$sponsor->price}}€</strong> per {{$sponsor->duration}}/h</p>
+          <div class="bg-white rounded">
+            <a href="" class="btn w-100 fs-4 " style="background-image: linear-gradient(to left,{{$sponsor->color}}); -webkit-background-clip: text; -webkit-text-fill-color: transparent; background-clip: text; font-weight: 700;">Passa a pro</a>
+          </div>
         </div>
       </div>
     </div>
