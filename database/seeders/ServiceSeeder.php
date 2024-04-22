6f09a5f44bc881d336c5dc3d6bfb0cbcc8f7cae7
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-22 17:40:32 +0200

    updated show layout

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
 use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,8 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
+        $apartments_ids = Apartment::pluck('id')->toArray();
+
         // Services List
         $services = config('apartment_services');
 
@@ -24,6 +27,10 @@ class ServiceSeeder extends Seeder
             $new_service->icon = $service['icon'];
 
             $new_service->save();
+
+            $service_apartments = array_filter($apartments_ids, fn () => rand(0, 1));
+
+            $new_service->apartments()->attach($service_apartments);
         }
     }
 }
--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Appartamento')
 
 @section('content')
-  <h1 class="text-center my-5">{{$apartment->title}}</h1>
-  <hr>
-  <div class="d-flex justify-content-center">
+  {{-- <h1 class="text-center my-5">{{$apartment->title}}</h1>
+  <hr> --}}
+  {{-- <div class="d-flex justify-content-center">
     
       <div class="row g-3">
         <div class="col-md-5">
@@ -46,5 +46,82 @@
             <a href="{{route('admin.apartments.edit', $apartment->id)}}" class="btn btn-warning"><i class="fa-regular fa-pen-to-square"></i> Modifica</a>
           </div>
       </div>
+  </div> --}}
+
+  <div class="row mt-5">
+    <div class="col-8">
+      <section id="detail">
+        <div class="card p-4">
+
+          {{-- Titolo appartamento --}}
+          <h1 class="mb-4">{{$apartment->title}}</h1>
+
+          {{-- Immagine di copertina --}}
+          <div class="thumb-img">
+            <img src="{{$apartment->cover}}" alt="{{$apartment->title}}" class="img-fluid">
+          </div>
+          <div class="card-body">
+
+            {{-- Descrizione appartamento --}}
+            <h2 class="card-title mb-3">Descrizione appartamento</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur. In imperdiet iaculis enim lorem. Nibh nulla vulputate cursus ligula mauris. Rhoncus sit arcu accumsan sed tempor. Ut orci egestas at tincidunt. Faucibus odio diam aliquam odio neque aliquet massa. Nulla mauris nisl diam sit fusce. Turpis rutrum urna leo semper donec fermentum.
+              Tincidunt vel velit vulputate laoreet justo non hendrerit faucibus in pellentesque.
+
+              {{-- Indirizzo --}}
+              <div class="road fw-bold"><i class="fa-solid fa-location-dot me-2"></i>{{$apartment->address}}</div>
+            </p>
+
+            {{-- Dettaglio appartamento --}}
+            <h2 class="card-title mt-5 mb-2">Dettaglio appartamento</h2>
+            <div class="fw-medium stats mb-2">Come è composto</div>
+            <ul class="d-flex gap-3">
+              <li>
+                <span class="badge fw-medium"><i class="fa-solid fa-door-closed me-2"></i> {{$apartment->rooms}} camere</span>
+              </li>
+              <li>
+                <span class="badge fw-medium"><i class="fa-solid fa-bath me-2"></i> {{$apartment->bathrooms}} bagni</span>
+              </li>
+              <li>
+                <span class="badge fw-medium"><i class="fa-solid fa-ruler-horizontal me-2"></i> {{$apartment->sqm}} mq</span>
+              </li>
+              <li>
+                <span class="badge fw-medium"><i class="fa-solid fa-bed me-2"></i> {{$apartment->rooms}} letti</span>
+              </li>
+            </ul>
+
+            {{-- Statistiche --}}
+            <div class="fw-medium stats mt-4 mb-2">Statistiche</div>
+            <ul class="d-flex gap-3">
+              <li>
+                <span class="badge fw-medium"><i class="fa-regular fa-eye me-2"></i> 4523 Visualizzazioni</span>
+              </li>
+              <li>
+                <span class="badge fw-medium"><i class="fa-regular fa-envelope me-2"></i> 12 Messaggi</span>
+              </li>
+            </ul>
+
+            {{-- Stato pubblicazione --}}
+            <div class="mt-5 fs-5">{!!$apartment->is_visible ? '<i class="fa-solid fa-circle-check text-success me-2"></i> <span class="text-success fw-bold">Pubblicato</span>' : '<i class="fa-solid fa-circle-xmark text-danger"></i> <span class="text-danger fw-bold">Non pubblicato</span>'!!}</div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="col-4">
+      <section id="services">
+        <div class="card p-3">
+          <div class="card-body">
+            <h2 class="card-title mb-4">Servizi appartamento</h2>
+            <ul class="p-0 m-0">
+              @foreach ($services as $service)
+              @if ($apartment->services)
+                  <h1>ciao</h1>
+              @endif
+              <li class="fw-medium mb-3"><img src="" alt="">{{$service->label}}</li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 @endsection
