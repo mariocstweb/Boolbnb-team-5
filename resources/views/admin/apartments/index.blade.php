f9b1bf5539dc74879052b9793daddcfa0a74edf6
mariocstweb <mariocstmra@gmail.com>
2024-04-22 12:12:16 +0200

    added scss

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -10,15 +10,15 @@
   {{-- Barra di ricerca --}}
   <form method="GET" action="{{ route('admin.apartments.index')}}">
     <div class="d-flex">
-        <input type="search" class="form-control me-2" placeholder=" Cerca..." name="search"
+        <input type="search" class="form-control" placeholder=" Cerca..." name="search"
         value="{{ $search }}">
-        <button class="btn text-white bg-main" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button class="btn text-white bg-hover" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </div>
   </form>
 
   <div>
     {{-- Aggiungi appartamento --}}
-    <a href="{{route('admin.apartments.create')}}" class="btn bg-main me-2 text-white p-2"><i class="fa-solid fa-house-medical ms-1"></i> Aggiungi appartamento</a>
+    <a href="{{route('admin.apartments.create')}}" class="btn bg-hover me-2 text-white p-2"><i class="fa-solid fa-house-medical ms-1"></i> Aggiungi appartamento</a>
 
     {{-- Cestino --}}
     <a class="btn c-main p-2 btn-sec" href="{{route('admin.apartments.trash')}}"><i class="fa-regular fa-trash-can"></i> Cestino</a>  
@@ -31,12 +31,12 @@
   <table class="table table-hover">
     <thead class="border-top">
       <tr>
-        <th scope="col" class="text-start">Anteprima</th>
+        <th scope="col">Anteprima</th>
         <th scope="col">ID</th>
         <th scope="col">Nome Appartamento</th>
         <th scope="col">Pubblicato</th>
         <th scope="col">Sponsorizzazione</th>
-        <th scope="col" class="text-start">Azioni</th>
+        <th scope="col">Azioni</th>
       </tr>
     </thead>
     <tbody>
@@ -44,7 +44,7 @@
         <tr>
 
           {{-- Cover --}}
-          <td class="text-start"><img src="{{$apartment->cover}}" alt="{{$apartment->title}}" class="img-fluid table-img rounded-1"></td>
+          <td><img src="{{$apartment->cover}}" alt="{{$apartment->title}}" class="img-fluid table-img rounded-1"></td>
 
           {{-- ID --}}
           <td>{{$apartment->id}}</td>
