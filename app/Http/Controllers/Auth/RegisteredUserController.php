ced2024d463392547352cf1591b69fd67b66a839
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-23 11:29:30 +0200

    added validation messages

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,29 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'birthday' => ['required', 'date', 'before_or_equal:' . now()->subYears(18)->format('Y-m-d')],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'name.required' => 'Il nome è obbligatorio',
+            'name.max' => 'Il nome deve avere un massimo di :max caratteri',
+            'birthday.required' => 'Il campo data di nascita è obbligatorio.',
+            'birthday.date' => 'Il campo data di nascita deve essere una data valida.',
+            'birthday.before_or_equal' => 'Devi avere almeno 18 anni per registrarti.',
+            'email.required' => 'Il campo email è obbligatorio.',
+            'email.string' => 'Il campo email deve essere una stringa.',
+            'email.lowercase' => 'L\'email deve essere in minuscolo.',
+            'email.email' => 'Il campo email deve essere un indirizzo email valido.',
+            'email.max' => 'Il campo email non può superare i 255 caratteri.',
+            'email.unique' => 'L\'indirizzo email specificato è già stato utilizzato.',
+            'password.required' => 'Il campo password è obbligatorio.',
+            'password.confirmed' => 'La conferma della password non corrisponde.',
+            'password.password' => 'La password deve contenere almeno otto caratteri, includendo almeno una lettera maiuscola, una lettera minuscola, un numero e un carattere speciale.'
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'birthday' => $request->birthday,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
