36a6a1186a4b51dc5800d8897c185f09adb13e4c
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-26 11:17:27 +0200

    edit grafic profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,28 +5,49 @@
 @section('content')
 
     <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Profile') }}
-        </h2>
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+        <h1 class=" my-4">
+            {{ __('Profilo') }}
+        </h1>
+        <hr>
+        <div class="row mb-5">
+            <div class="col-6 mt-5 d-flex justify-content-center align-items-center">
+                <div class="profile-photo ">
+                    <img src=" https://marcolanci.it/boolean/assets/placeholder.png" alt="foto-profilo" class="w-100 rounded-circle">
 
-            @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
+            <div class="col-6">
 
+                <div class="card p-4 bg-white shadow rounded-lg">
+
+                    @include('profile.partials.update-profile-information-form')
+        
+                </div>
+            </div>
         </div>
+        <div class="row">
 
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+            <div class="col-6">
+        
+                <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+        
+        
+                    @include('profile.partials.update-password-form')
+        
+                </div>
+            </div>
+            <div class="col-6">
 
-
-            @include('profile.partials.update-password-form')
-
+                <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+        
+        
+                    @include('profile.partials.delete-user-form')
+        
+                </div>
+            </div>
+        
         </div>
-
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-            @include('profile.partials.delete-user-form')
-
-        </div>
+        
     </div>
 
 @endsection
