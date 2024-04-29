31de1a373310d4a157865dc7d6d9205a64924b3a
mariocstweb <mariocstmra@gmail.com>
2024-04-29 17:18:07 +0200

    filled Api ApartmentController

--- app/Http/Controllers/Api/ApartmentsController.php
+++ app/Http/Controllers/Api/ApartmentsController.php
@@ -12,9 +12,41 @@ class ApartmentsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    // public function index()
+    // {
+    //     $apartments = Apartment::latest()->paginate(5);
+
+    //     return response()->json($apartments);
+    // }
+
+    public function index(Request $request)
     {
-        $apartments = Apartment::latest()->paginate(5);
+        $query = Apartment::query();
+
+        // Filtra gli appartamenti per indirizzo se presente il parametro "address"
+        if ($request->has('address')) {
+            $address = $request->input('address');
+            $query->where('address', 'like', '%' . $address . '%');
+        }
+
+        // Filtra gli appartamenti per range se presente il parametro "range"
+        if ($request->has('range') && $request->has('latitude') && $request->has('longitude')) {
+            $range = $request->input('range');
+            $latitude = $request->input('latitude');
+            $longitude = $request->input('longitude');
+
+            $query->selectRaw(
+                '( 6371 * acos( cos( radians(?) ) *
+                cos( radians( latitude ) ) *
+                cos( radians( longitude ) - radians(?) ) +
+                sin( radians(?) ) *
+                sin( radians( latitude ) ) ) ) AS distance',
+                [$latitude, $longitude, $latitude]
+            )->havingRaw("distance < ?", [$range]);
+        }
+
+        // Ordina gli appartamenti per data di creazione, paginazione con 5 risultati per pagina
+        $apartments = $query->latest()->paginate(5);
 
         return response()->json($apartments);
     }
