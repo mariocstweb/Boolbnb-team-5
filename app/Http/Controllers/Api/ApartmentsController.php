1105ff20a46cf21c63bd016b4fd48cfb7265fe41
mariocstweb <mariocstmra@gmail.com>
2024-04-30 10:33:05 +0200

    filled show in Api/ApartmentController

--- app/Http/Controllers/Api/ApartmentsController.php
+++ app/Http/Controllers/Api/ApartmentsController.php
@@ -10,16 +10,6 @@ use Illuminate\Http\Request;
 
 class ApartmentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    // public function index()
-    // {
-    //     $apartments = Apartment::latest()->paginate(5);
-
-    //     return response()->json($apartments);
-    // }
-
     public function index(Request $request)
     {
         $query = Apartment::query();
@@ -49,35 +39,18 @@ class ApartmentsController extends Controller
             });
         }
 
-
-        // // Filtra gli appartamenti per range se presente il parametro "range"
-        // if ($request->has('range') && $request->has('latitude') && $request->has('longitude')) {
-        //     $range = $request->input('range');
-        //     $latitude = $request->input('latitude');
-        //     $longitude = $request->input('longitude');
-
-        //     $query->selectRaw(
-        //         '( 6371 * acos( cos( radians(?) ) *
-        //         cos( radians( latitude ) ) *
-        //         cos( radians( longitude ) - radians(?) ) +
-        //         sin( radians(?) ) *
-        //         sin( radians( latitude ) ) ) ) AS distance',
-        //         [$latitude, $longitude, $latitude]
-        //     )->havingRaw("distance < ?", [$range]);
-        // }
-
         // Ordina gli appartamenti per data di creazione, paginazione con 5 risultati per pagina
         $apartments = $query->latest()->with('services')->get();
         return response()->json($apartments);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        $apartment = Apartment::find($id);
+        $apartment = Apartment::with('services')->find($id);
+
         if (!$apartment) return response(null, 404);
+
         return response()->json($apartment);
     }
 }
