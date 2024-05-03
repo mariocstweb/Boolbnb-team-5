c7d345d0377c5a1ea42e7888b166ab8184b5bcb8
mariocstweb <mariocstmra@gmail.com>
2024-05-03 16:21:10 +0200

    fixed dashboard view/controller

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
+@extends('layouts.app')
+
+@section('title', 'Pannello di controllo')
+
+@section('content')
+
     <h1 class="mt-5 mb-5">Pannello di controllo</h1>
     <div class="row mb-5">
         <div class="col">
@@ -13,7 +19,7 @@
                 <div>
                     <span class="btn bg-icon me-2"><i class="fa-solid fa-house"></i></span>
                     {{-- TOTALE APPARTAMENTI --}}
-                    <span>{{ $apartments->total() }}</span>
+                    <span>{{ $apartments->count() }}</span>
                 </div>
             </div>
         </div>
@@ -27,7 +33,6 @@
                     {{-- <span>{{ $messages->count() }}</span> --}}
                     <span>{{ $totalMessages }}</span>
                 </div>
-
             </div>
         </div>
         <div class="col">
@@ -45,7 +50,7 @@
     <h1 class="mb-5">Statistiche</h1>
     <div class="row">
         <div class="col-6">
-            {{-- CARD VISUALIZZAZZIONI TOTALI--}}
+            {{-- CARD VISUALIZZAZIONI TOTALI--}}
             <div class="card p-3">
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h4>Visualizzazioni</h4>
@@ -71,70 +76,61 @@
     </div>
 
 
+
 @endsection
 @section('scripts')
-      <!--Import cdn ChartJS-->
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js/dist/Chart.min.js"></script>
 
-      <script>
-          //*** FUNCTIONS ***//
-  
-          /**
-           * Inizialize a Chart JS Graph
-           */
-          const initGraph = (elem, title, labels, data, backgroundColor) => {
-              const graph = new Chart(elem, {
-                  type: 'bar',
-                  data: {
-                      labels,
-                      datasets: [{
-                          label: title,
-                          data,
-                          borderWidth: 1,
-                          backgroundColor
-                      }]
-                  },
-                  options: {
-                      scales: {
-                          y: {
-                              beginAtZero: true
-                          }
-                      },
-                      maintainAspectRatio: false,
-                      plugins: {
-                          legend: {
-                              display: false
-                          }
-                      }
-                  }
-              });
-  
-              return graph;
-          }
-  
-  
-          //*** INIT ***//
-          // Axis Data
-          const monthsAxis = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre',
-              'Ottobre', 'Novembre', 'Dicembre'
-          ];
-          const yearsAxis = ['2021', '2022', '2023', '2024'];
-  
-          // Get DOM Elems
-          const viewsTotalElem = document.getElementById('total-views');
-          const messagesTotalElem = document.getElementById('total-messages');
-         
-  
-          // Get Data From PHP
-          const viewsTotalData = <?php echo json_encode($month_views) ?>;
-          const messagesTotalData = <?php echo json_encode($month_messages) ?>;
-        
-          // Init All Views Graphs
-          initGraph(viewsTotalElem, 'Visualizzazioni totali', monthsAxis, object.values(viewsTotalData), '#dc3545');
-         
-  
-          // Init All Messages Graphs
-          
-          initGraph(messagesTotalElem, 'Messaggi totali', monthsAxis,  object.values(messagesTotalData));
-      </script>
+<script>
+    // Funzione per inizializzare un grafico Chart JS
+    const initGraph = (elem, title, labels, data, backgroundColor) => {
+        const graph = new Chart(elem, {
+            type: 'bar',
+            data: {
+                labels,
+                datasets: [{
+                    label: title,
+                    data,
+                    borderWidth: 1,
+                    backgroundColor
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }
+        });
+
+        return graph;
+    }
+
+    // Assegni le variabili per gli assi dei mesi e dei anni
+    const monthsAxis = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre',
+        'Ottobre', 'Novembre', 'Dicembre'
+    ];
+
+    // Ottieni gli elementi DOM dei grafici
+    const viewsTotalElem = document.getElementById('total-views');
+    const messagesTotalElem = document.getElementById('total-messages');
+
+    // Ottieni i dati delle visualizzazioni e dei messaggi
+    const viewsTotalData = <?php echo json_encode($month_views) ?>;
+    const messagesTotalData = <?php echo json_encode($month_messages) ?>;
+
+    // Inizializza il grafico delle visualizzazioni totali
+    initGraph(viewsTotalElem, 'Visualizzazioni totali', monthsAxis, viewsTotalData, '#dc3545');
+
+    // Inizializza il grafico dei messaggi totali
+    initGraph(messagesTotalElem, 'Messaggi totali', monthsAxis, messagesTotalData);
+</script>
+
 @endsection
