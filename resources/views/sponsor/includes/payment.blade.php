4e12837daba77583a465175ea785b0060782f41a
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-05-08 15:20:14 +0200

    responsive sponsor

--- resources/views/sponsor/includes/payment.blade.php
+++ resources/views/sponsor/includes/payment.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Selezione il tuo sponsor</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Seleziona il tuo pacchetto</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
