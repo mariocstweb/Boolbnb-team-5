616bc7ab901e4cc553c01898d0a7d73fc6ddf1d0
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-05-08 15:43:35 +0200

    responsive messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -4,25 +4,24 @@
 
 @section('content')
 
-    <div class="container my-5">
+    
 
-        <div class="card p-3 shadow">
+        <div class="card p-3 m-3 shadow">
             {{-- TITOLO --}}
             <h2>Lista Messaggi</h2>
 
             {{-- TABELLA --}}
-            <table class="table table-white table-hover align-middle mt-5">
+            <table class="table table-white table-hover  mt-5">
     
                 {{-- HEADER TABELLA --}}
                 <thead>
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Titolo</th>
-                        <th scope="col">Contenuto</th>
+                        <th scope="col" class="d-none d-lg-table-cell">Contenuto</th>
                         <th scope="col">Email</th>
                         <th scope="col">Appartamento</th>
                         <th scope="col" class="d-none d-lg-table-cell">Data</th>
-                        <th scope="col"></th>
                     </tr>
                 </thead>
     
@@ -37,7 +36,7 @@
                             <td>{{ $message->name }}</td>
     
                             {{-- CONTENUTO --}}
-                            <td>{{ $message->text }}</td>
+                            <td class="d-none d-lg-table-cell">{{ $message->text }}</td>
     
                             {{-- EMAIL --}}
                             <td>{{ $message->email }}</td>
@@ -52,7 +51,7 @@
                         {{-- ALTRIMENTI --}}
                     @empty
                         <tr>
-                            <td class="text-center" colspan="8">
+                            <td class="text-center" colspan="7">
                                 <h3>Non ci sono messaggi</h3>
                             </td>
                         </tr>
@@ -61,6 +60,6 @@
             </table>
         </div>
 
-    </div>
+    
 
 @endsection
